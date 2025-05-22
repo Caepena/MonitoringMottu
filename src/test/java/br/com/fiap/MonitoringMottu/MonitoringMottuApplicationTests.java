1e392f9982c555e7c847cb0867e1bd1f09295d0d
Caepena <caepenafiel@gmail.com>
2025-05-22 08:30:34 -0300

    Projeto iniciado

---
+++ src/test/java/br/com/fiap/MonitoringMottu/MonitoringMottuApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.MonitoringMottu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringMottuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
